b64944da0ce0f4fefa32747c75c4583995f64138
Enad Germani <33943826+siamoInPochi@users.noreply.github.com>
2019-05-12 19:27:06 +0200

    Update HTMLreader.java

--- src/main/java/Ilsottomarinobot/HTMLreader.java
+++ src/main/java/Ilsottomarinobot/HTMLreader.java
@@ -1,106 +1,136 @@
-
-
 package com.amazonaws.lambda.demo;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
+//manda con calma no html
+//Se prima manda tutto insieme, dopo manda_con_calma tiene l'rdine invertito
+
+
+
+
+
+
+
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class HTMLreader {
+
     public HTMLreader() {
+
     }
 
     public HTMLreader(String link) {
+
+
     }
 
-    public static ArrayList<String> agisci(String start, String end) {
+
+     public static ArrayList<String> agisci(String start, String end) {
+
+
         String prova = findLink();
         System.out.println(prova);
-        Document d = null;
 
+        Document d = null;
         try {
             d = Jsoup.connect(prova).get();
-        } catch (IOException var21) {
-            var21.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         String html = d.toString();
-        Document d2 = null;
 
+
+        Document d2 = null;
         try {
             d2 = Jsoup.connect(prova).get();
-        } catch (IOException var20) {
-            var20.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         String source = d2.toString();
         System.out.println("seconda html trovata");
+
         int inizio = source.indexOf(start);
+
         int fine = source.indexOf(end);
-        String cut = source.substring(inizio + 188, fine - 3) + "§";
+
+        String cut = source.substring((inizio + 188), (fine - 3)) + "§";
+
         System.out.println("inizio" + inizio);
         System.out.println("fine" + fine);
+        //  System.out.println(cut);
+
         Document doc = Jsoup.parse(cut);
         Elements p = doc.select("p");
-        ArrayList<String> articoli = new ArrayList();
+
+        ArrayList<String> articoli = new ArrayList<String>();
         String s = "";
         String s1 = "";
-        Iterator var16 = p.iterator();
-        
-        while(var16.hasNext()) {
-            Element par = (Element)var16.next();
+        for (Element par : p) {
             s = par.text();
             Elements a = par.select("a[href]");
-
-            for(Iterator var19 = a.iterator(); var19.hasNext(); s1 = "") {
-                Element links = (Element)var19.next();
+            for (Element links : a) {
                 s1 = s1.concat("  ");
                 s1 = s1.concat(links.attr("href"));
+                //      System.out.println(s + s1);
                 articoli.add(s + s1 + "¿º");
                 s = "";
+                s1 = "";
             }
         }
-
         return articoli;
-    }
 
+    }
     public static String findLink() {
+
         System.out.println("cerco il link");
         String base = "https://thesubmarine.it/author/redazione/";
-        Document d = null;
+        //   wd.get(base);
+        //   String html = wd.getPageSource();
+        //  System.out.println(html);
 
+        Document d = null;
         try {
             d = Jsoup.connect(base).get();
-        } catch (IOException var13) {
-            var13.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         String html = d.toString();
         String html2 = html.substring(html.indexOf("div id=\"cb-content\" class=\"wrap cb-author-page cb-wrap-pad clearfix\">"));
+
         Document doc = Jsoup.parse(html2);
         Elements p = doc.select("h2");
+
+        //     ArrayList<String> articoliRedazione = new ArrayList<String>();
         String s = "";
         String s1 = "";
-        Iterator var9 = p.iterator();
-        if (!var9.hasNext()) {
-            return "ciao";
-        } else {
-            Element par = (Element)var9.next();
+        for (Element par : p) {
             s = par.text();
             Elements a = par.select("a[href]");
-
-            Element links;
-            for(Iterator var12 = a.iterator(); var12.hasNext(); s1 = s1.concat(links.attr("href"))) {
-                links = (Element)var12.next();
+            for (Element links : a) {
                 s1 = s1.concat("");
+                s1 = s1.concat(links.attr("href"));
             }
-
             System.out.println("link non ancora trovato");
             return s1;
+            // s = "";
+            //    s1 = "";
         }
+
+
+        return "ciao";
     }
 }
+
+
+
+
+
+
+
+
